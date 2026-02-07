ff8089f81d796f52d34d0c41da47440e9d4e85da
BetaBlues <olivia271@smapdx.org>
2026-02-07 10:46:37 -0800

    Co-authored-by: whoopssmhu <whoopssmhu@users.noreply.github.com>

---
+++ fishtank_2026/src/main/java/frc/robot/Subsystems/SwerveEncoder.java
@@ -0,0 +1,66 @@
+// package frc.robot.Subsystems;
+// import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+// public class SwerveEncoder extends DutyCycleEncoder{
+    
+
+//     public SwerveEncoder(int port) {
+//         super(port);
+        
+//     }
+
+//     public SwerveEncoder(int port, double fullRange, double expectedZero) {
+//         super(port, expectedZero, fullRange);
+       
+//     }
+//     public double get() {
+//         return super.get();
+//     }
+
+    
+
+    
+// }
+
+package frc.robot.Subsystems;
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+import com.ctre.phoenix6.hardware.core.CoreCANcoder;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.units.Units;
+
+//(device_id: int, canbus: phoenix6.canbus.CANBus | str = CANBus())
+
+public class SwerveEncoder extends CoreCANcoder {
+
+    private double fullRange;
+    private double expectedZero;
+
+    public SwerveEncoder(int port) {
+        super(port);
+        this.fullRange = 1.0;
+        this.expectedZero = 0.0;
+    }
+
+    public SwerveEncoder(int port, double fullRange, double expectedZero) {
+        super(port, CANBus.roboRIO());
+        this.fullRange = fullRange;
+        this.expectedZero = expectedZero;
+       
+    }
+    public double get() {
+        // StatusSignal<Angle> signal = super.getAbsolutePosition();
+        // return signal.getValue().in(Radians);
+        double rawValue = super.getAbsolutePosition().getValue().in(Units.Radians);
+        rawValue /= 2 * Math.PI; // Convert from radians to a 0-1 range
+        return rawValue;
+    }
+
+    
+
+    
+}
